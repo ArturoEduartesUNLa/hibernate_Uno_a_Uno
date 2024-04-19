6a09aa1ecc837fe256d9e09b3a8b0ebefa14ffec
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-04-18 22:42:06 -0300

    Test Modificar contacto in progress

--- src/test/TestAgregarCliente.java
+++ src/test/TestAgregarCliente.java
@@ -30,27 +30,5 @@ public class TestAgregarCliente {
 			e.printStackTrace();
 		}
 		
-		/*
-		Cliente cliente = abmCliente.traer(14000001);
-		System.out.println(cliente);
-		cliente.setNombre("pirulo1");
-	
-		cliente.setContacto(new Contacto("CharlyG@music", "9876543", "Re_fijo"));
-		  try { 
-			  abmCliente.modificar(cliente);
-		  System.out.println(abmCliente.traer(2L)); } catch (Exception e) {
-		  e.printStackTrace(); }
-		
-
-		try {
-			long idClie = abmCliente.agregar("1", "2", 14000004, LocalDate.of(1960, 9, 10), false,
-					new Contacto("jnicholson@unla.edu.ar", "22-2222-2222", "022-2222-2222"));
-			System.out.println(abmCliente.traer(idClie));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		*/
-
 	}
 }
