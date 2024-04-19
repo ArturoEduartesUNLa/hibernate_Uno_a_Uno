1efc6fd9db3070ffcfe1e4b5072325d8ae5b0afa
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-04-19 18:06:00 -0300

    Test modifica Contacto - completed

--- src/test/TestModificarContacto.java
+++ src/test/TestModificarContacto.java
@@ -1,38 +1,24 @@
 package test;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 
-import datos.Cliente;
 import datos.Contacto;
-import negocio.ClienteABM;
 import negocio.ContactoABM;
 
+/* COMPLETE
+ * 
+ */
 public class TestModificarContacto {
 
 	public static void main(String[] args) {
-		ClienteABM abmCliente = new ClienteABM();
-		ContactoABM abmContacto = new ContactoABM();
+		System.out.println(new ContactoABM().traer(2)); // traer por IdContacto
 
-		Cliente cliente = abmCliente.traer(14000001);
-		System.out.println(cliente);
-		cliente.setNombre("pirulo1");
-	
-		cliente.setContacto(new Contacto("CharlyG@music", "9876543", "Re_fijo"));
-		  try { 
-			  abmCliente.modificar(cliente);
-		  System.out.println(abmCliente.traer(2L)); } catch (Exception e) {
-		  e.printStackTrace(); }
-		
+		// modificar
+		Contacto contactoMod = new ContactoABM().traer(2);
+		contactoMod.setEmail(contactoMod.getEmail() + "_" + LocalTime.now().getSecond());
 
-		try {
-			long idClie = abmCliente.agregar("1", "2", 14000004, LocalDate.of(1960, 9, 10), false,
-					new Contacto("jnicholson@unla.edu.ar", "22-2222-2222", "022-2222-2222"));
-			System.out.println(abmCliente.traer(idClie));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		
+		new ContactoABM().modificar(contactoMod);
+		System.out.println(new ContactoABM().traer(2)); // traer por IdContacto
 
 	}
 }
